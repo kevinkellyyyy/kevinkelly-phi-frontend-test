63836fa412cb0ecab87b4ed2bf9d75112f743209
kevinkellyyyy <kellyisyanta@gmail.com>
2020-07-23 21:00:51 +0700

    Updates

---
+++ precache-manifest.855c9765ba6a64241998f48b0dd4d0ec.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0764b5324f1414852bba031ef592d10",
+    "url": "/kevinkelly-phi-frontend-test/index.html"
+  },
+  {
+    "revision": "8c322a3f2b5829af10a8",
+    "url": "/kevinkelly-phi-frontend-test/static/css/main.fa288a90.chunk.css"
+  },
+  {
+    "revision": "d2bb83cb07eacc83595b",
+    "url": "/kevinkelly-phi-frontend-test/static/js/2.6ac0a75d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kevinkelly-phi-frontend-test/static/js/2.6ac0a75d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c322a3f2b5829af10a8",
+    "url": "/kevinkelly-phi-frontend-test/static/js/main.3dbdb3a5.chunk.js"
+  },
+  {
+    "revision": "cbbc0aa3eadfb4364485",
+    "url": "/kevinkelly-phi-frontend-test/static/js/runtime-main.44bcec2a.js"
+  },
+  {
+    "revision": "b53c640665c53f9bebacfbd7b42a55c8",
+    "url": "/kevinkelly-phi-frontend-test/static/media/bg1.b53c6406.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/kevinkelly-phi-frontend-test/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/kevinkelly-phi-frontend-test/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/kevinkelly-phi-frontend-test/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/kevinkelly-phi-frontend-test/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/kevinkelly-phi-frontend-test/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kevinkelly-phi-frontend-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
